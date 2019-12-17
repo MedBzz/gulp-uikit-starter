7bba5f5d459b1df66fade5e44cb4c09ed72885b9
Medbzz <medbouazzi44@gmail.com>
2019-12-17 22:00:05 +0100

    Update

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Gulp 4 and Uikit 3 starter
+# Uikit 3 starter with Gulp Js 4
+
+Kick-start your project with [Uikit 3](https://getuikit.com/), A lightweight and modular front-end framework for developing fast and powerful web interfaces. This boilerplate with [Gulp Js 4](https://gulpjs.com/) based setup helps you build web apps and sites much faster.
+## Theme development
+[Node.js](http://nodejs.org/) is the only required dependency to work with _HTML Starter_.
+
+#### [](https://github.com/MedBzz/gulpJs-uikit-starter.git#installation)Installation
+
+1.  Install [Node.js](http://nodejs.org/) (installation depends on your system). After finishing, you will be able to check the version number using `node -v` and `npm -v` commands ([npm is distributed with Node.js](https://www.npmjs.com/get-npm)).
+2.  It is recommended to install the latest version of [Yarn](https://yarnpkg.com/en/docs/install).
+3.  Clone the repo using `git clone https://github.com/MedBzz/gulpJs-uikit-starter.git` or [download _Uikit 3 Starter_](https://github.com/MedBzz/gulpJs-uikit-starter/archive/master.zip).
+4.  Open folder **gulpJs-uikit-starter** (command: `cd gulpJs-uikit-starter`) and install a packages of _HTML Starter_ via command:  `npm install`.
+
+Now you have everything you need to run the build process.
+
+#### Build commands
+
+-   `npm run watch` ─ compile assets when file changes are made, start session
+-   `npm run build` ─ compile for production
---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+const { src, dest, parallel, watch } = require('gulp');
+const pug = require('gulp-pug');
+const sass = require("gulp-sass");
+const less = require('gulp-less');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const minifyCSS = require('gulp-csso');
+const browserSync = require('browser-sync').create();
+
+function html() {
+    return src('src/views/*.pug')
+    .pipe(pug({pretty:true}))
+    .pipe(dest('dist'))
+}
+
+// function css() {
+//   return src('./src/assets/less/*.less')
+//     .pipe(less())
+//     .pipe(concat('app.min.css'))
+//     .pipe(minifyCSS())
+//     .pipe(dest('dist/css'))
+// }
+
+function css() {
+    return src("./src/assets/scss/*.scss")
+    .pipe(sass()).on("error", sass.logError)
+    .pipe(concat('app.min.css'))
+    .pipe(minifyCSS())
+    .pipe(dest('dist/assets/css'))
+    .pipe(browserSync.stream());
+}
+
+function js() {
+    return src([
+        './src/assets/js/*.js', 
+        './node_modules/uikit/dist/js/uikit.js', 
+        './node_modules/uikit/dist/js/uikit-icons.js'
+    ], { sourcemaps: true })
+    .pipe(uglify())
+    .pipe(concat('app.min.js'))
+    .pipe(dest('dist/assets/js', { sourcemaps: true }))
+}
+
+exports.js = js;
+exports.css = css;
+exports.html = html;
+exports.build = parallel(html, css, js);
+
+
+exports.watch = function() {
+    browserSync.init({
+        server: {
+           baseDir: "./dist",
+           index: "/index.html"
+        }
+    });
+    watch('./src/assets/js/**/*.js', { events: 'all', ignoreInitial: false }, js).on('change', browserSync.reload);
+    watch('./src/views/**/*.pug', { events: 'all', ignoreInitial: false }, html).on('change', browserSync.reload);
+    watch('./src/assets/scss/**/*.scss', { events: 'all', ignoreInitial: false }, css);
+};
